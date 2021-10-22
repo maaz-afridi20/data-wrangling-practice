e6b4403ea0dae0dc3ea21b0714e9263f2027e1e6
maaz-afridi20 <maazafridi.ma47@gmail.com>
2021-10-22 15:25:48 +0500

    Reading Tables ANd CSV's files

--- simple.R
+++ simple.R
@@ -65,9 +65,63 @@ setwd('../')
     # we can save or download the date that on which date the file is
     # dowloaded of not. like
     
+    camera <- read.csv('cameras.csv')
 
-
-
+# -------------------------------------------------
+    
+    # Reading Text and csv files : (tables and csv)
+    
+    # by the read.table() we can read data from table
+    # but for this we have to give some arguments like
+    # firest we have to give path of the file and then 
+    # if that file consists any columns name then we have
+    # a parameter called header so we have to make it true
+    
+    # in some files the data is seprated by some thing like may
+    # be in some time it is seprated by , sometime it is 
+    # seprated by ; or any thing like that so for this we have one
+    # thing sep which stands for seprator so in reading table we
+    # have to specify that  like below 
+    
+    
+    camera <- read.table("cameras.csv", sep = ",", header = TRUE)
+    View(head(camera))
+    
+    # some times there is na values in the data so or there is
+    # empty space inside the data snd R by default put na inside that
+    # emplty spaces so for example if we want that we do not want
+    # to put na and we want another thing like we have to put $ suybol
+    # or any other thing (in string) so for that we have options
+    # which  na.strings() in this we can give some thing that we 
+    # want to put there
+    
+    
+    
+    
+    # read.csv() and read.csv2() :
+    
+    # there are 2 types of read.csv i-e read.csv() and read.csv2()
+    # so the main diff is that when we need speedy results then 
+    # we use read.csv2() meand that read.csv2() is fast than 
+    # read.csv()
+    # 
+    
+    #fread : 
+        
+        # another alternate for both of the read.csv() and 
+        # read.csv2() is fread which is more faster tahn both
+        # of them and for this we have to load library which is 
+        library(data.table)
+        data = fread('cameras.csv')
+        
+        #for fread() there are many attributes that we can Use
+        
+    
+    
+    
+    
+    
+    
 
 
 
