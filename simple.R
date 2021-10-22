ffa65253a73276a823747ed212b6db9b1b09c50c
maaz-afridi20 <maazafridi.ma47@gmail.com>
2021-10-22 11:25:24 +0500

    Downlading Files

--- simple.R
+++ simple.R
@@ -41,6 +41,45 @@ setwd('../')
 # this will bring back us to data-wrangling-practice directory
 
 
+# -------------------------------------
+
+    # if we want to download some data we can use download.file()
+    # the advantage of this download.file() command is taht when
+    # we downlaod some file manually and then after some time there
+    # come some changes in that file so when we use this command this will
+    # download all of its newly added addition 
+    
+    # so with this command we have to use 3 parameters which is 
+    # url, destfile (destination file where to file tobe downloaded) and
+    # the method 
+    
+    
+    fileUrl <- "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2020-financial-year-provisional/Download-data/annual-enterprise-survey-2020-financial-year-provisional-csv.csv"
+    download.file(fileUrl, destfile = './data',method = "curl")
+
+    
+    dateDownload <- date()
+
+    list.files('./data') # this will show files inside data folder
+    
+    # we can save or download the date that on which date the file is
+    # dowloaded of not. like
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
